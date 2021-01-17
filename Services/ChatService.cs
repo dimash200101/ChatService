fae973b00a14f0be48ff782771b3e0730db61758
dimash200101 <71224229+dimash200101@users.noreply.github.com>
2021-01-17 18:56:21 +0600

    Add files via upload

---
+++ Services/ChatService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatService01
+{
+    public class ChatService : ChatRoom.ChatRoomBase
+    {
+        private readonly Server.ChatRoom _chatroomService;
+
+        public ChatService(Server.ChatRoom chatRoomService)
+        {
+            _chatroomService = chatRoomService;
+        }
+
+        public override async Task join(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext()) return;
+
+            do
+            {
+                _chatroomService.Join(requestStream.Current.User, responseStream);
+                await _chatroomService.BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+
+            _chatroomService.Remove(context.Peer);
+
+        }
+
+
+    }
+}
